d64756052801afbf6f7e48aa649f29ca3ff4ed0a
Pierre Donias <pierre.donias@gmail.com>
2017-03-31 16:25:55 +0200

    fix: bin

---
+++ bin/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var run = require('../dist').default
+
+run()
--- src/index.js
+++ src/index.js
@@ -15,50 +15,52 @@ import {
 
 const { destinationPath } = args
 
-forEach(config, ({
-  cron,
-  delay,
-  description,
-  digits: nbDigits = 3,
-  firstIndex = 1,
-  name,
-  path,
-  url
-}) => {
-  const fileFolderPath = `${destinationPath}/${path}`
-  mkdirp(fileFolderPath, error => {
-    if (error) {
-      log(`ERROR while creating folder ${fileFolderPath}: ${error}`)
-    }
-  })
+export default function () {
+  forEach(config, ({
+    cron,
+    delay,
+    description,
+    digits: nbDigits = 3,
+    firstIndex = 1,
+    name,
+    path,
+    url
+  }) => {
+    const fileFolderPath = `${destinationPath}/${path}`
+    mkdirp(fileFolderPath, error => {
+      if (error) {
+        log(`ERROR while creating folder ${fileFolderPath}: ${error}`)
+      }
+    })
 
-  const onTick = () => {
-    dl(
-      url
-    ).then(data => {
-      const index = digits(String(firstIndex++), nbDigits)
-      const date = Date.now()
+    const onTick = () => {
+      dl(
+        url
+      ).then(data => {
+        const index = digits(String(firstIndex++), nbDigits)
+        const date = Date.now()
 
-      fs.writeFileSync(
-        `${fileFolderPath}/` + name.replace(/#|{date}/g, match => {
-          switch (match) {
-            case '#':
-              return index
-            case '{date}':
-              return date
-          }
-        }),
-        data
+        fs.writeFileSync(
+          `${fileFolderPath}/` + name.replace(/#|{date}/g, match => {
+            switch (match) {
+              case '#':
+                return index
+              case '{date}':
+                return date
+            }
+          }),
+          data
+        )
+      }).then(
+        () => log(`Saved ${description || name}`),
+        error => log(`ERROR while saving ${description || name}: ${error}`)
       )
-    }).then(
-      () => log(`Saved ${description || name}`),
-      error => log(`ERROR while saving ${description || name}: ${error}`)
-    )
-  }
+    }
 
-  return new CronJob({
-    cronTime: cron || `0 0/${delay} * * *`,
-    onTick,
-    start: true
+    return new CronJob({
+      cronTime: cron || `0 0/${delay} * * *`,
+      onTick,
+      start: true
+    })
   })
-})
+}
